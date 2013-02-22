af1bcb1d60cc55c2caef93362360ae614b6c9ce1
cristoperalta <cristoph_lp@hotmail.com>
2013-02-22 15:08:07 +0000

    Prueba

---
+++ Despliegue/src/Alumno.java
@@ -0,0 +1,35 @@
+
+public class Alumno {
+
+	/**
+	 * @param args
+	 */
+	String nombre, nombre2, apellido;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void getNombre(String n) {
+		// TODO Auto-generated method stub
+		nombre = n;	
+	}
+	
+	public String setNombre() {
+		// TODO Auto-generated method stub
+		return nombre;	
+	}
+	
+	public String setNombre2() {
+		// TODO Auto-generated method stub
+		return nombre2;	
+	}
+	
+	public void getApellido(String a) {
+		// TODO Auto-generated method stub
+		apellido = a;	
+	}
+
+
+}
