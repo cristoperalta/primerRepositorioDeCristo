13d9ef55e3d71b208a5b176c130c5182317d5d62
cristoperalta2 <cristoph_lp@yahoo.es>
2013-02-27 13:54:06 +0000

    cambiocristo3

--- Despliegue/src/Alumno.java
+++ Despliegue/src/Alumno.java
@@ -4,7 +4,7 @@ public class Alumno {
 	/**
 	 * @param args Cristo David Peralta Hernández
 	 */
-	String nombre, nombre2, apellido;
+	String nombre, nombre2, apellido, cosasBonitas, cosasBonitas2;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -26,9 +26,14 @@ public class Alumno {
 		return nombre2;	
 	}
 	
+	public void getApellido(String a) {
+		// TODO Auto-generated method stub
+		cosasBonitas = a;	
+	}
+	
 	public void getCosasBonitas(String a) {
 		// Ese criiiisto con unicornios
-		apellido = a;	
+		cosasBonitas2 = a;	
 	}
 
 	public void getCosasBonitas2(String a) {
